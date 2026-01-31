b0dd7422ada373d458fc9d5d8e01282d458612b3
Myselfsayan <sayanghosh8702@gmail.com>
2026-01-31 17:43:23 +0530

    Utl code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (reqHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(reqHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+// const asyncHandler = (fn)=>{()=>{}} in next line we just remove the Curly Braces , Here it is called Higher Order Js function
+
+
+//===================THIS IS TRY-CATCH EXAMPLE===========================
+// const asyncHandler = (fn)=> async(req , res , next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
