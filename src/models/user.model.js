7a6b95637576e4ab1fbdbd73ca396f2fa2815be1
Myselfsayan <sayanghosh8702@gmail.com>
2026-02-04 15:41:25 +0530

    Done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose , {Schema} from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new Schema(
     {
@@ -17,7 +19,7 @@ const userSchema = new Schema(
             lowercase : true,
             trim : true,
         },
-        fullNmae : {
+        fullName : {
             type:String,
             required : true, 
             trim : true,
@@ -49,6 +51,44 @@ const userSchema = new Schema(
         timestamps : true
     }
 )
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password"))    return next();
+
+    this.password = bcrypt.hash(this.password , 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            userName : this.userName,
+            fullName : this.fullName
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign(
+        {
+            _id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User",userSchema)
 //************  In mongoDB this 'User' will be saved as 'users'  ***************
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,12 +1,46 @@
 import mongoose , {Schema , model} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema (
     {
-        
+        videoFile : {
+            type : String, //cloudinary url
+            required : true
+        },
+        thumbnail : {
+            type : String, //cloudinary url
+            required : true
+        },
+        title : {
+            type : String,
+            required : true
+        },
+        description : {
+            type : String, 
+            required : true
+        },
+        duration : {
+            type : String, //cloudinary url
+            required : true
+        },
+        views : {
+            typr : Number,
+            default : 0
+        },
+        isPublished : {
+            typr : Boolean,
+            default : true
+        },
+        owner : {
+            typr : Schema.Types.ObjectId,
+            ref : "User"
+        },
     },
     {
         timestamps : true
     }
 )
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const video = model("Viedo",videoSchema)
