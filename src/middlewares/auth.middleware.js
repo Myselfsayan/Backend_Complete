0e80566a21e2fee3476f4f7ae7b0c087f66845ed
Myselfsayan <sayanghosh8702@gmail.com>
2026-03-18 21:12:39 +0530

    Remove unnecessary newline in JWT verification middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -27,6 +27,7 @@ export const verifyJWT = asyncHandler(async(req,_,next)=>{
         req.user = user;
         // console.log("USER:", req.user)
         //console.log("tknnnn",accessToken)
+        //
         next()
     } catch (error) {
         throw new ApiError(401,error?.message || "Invalid Access Token")
