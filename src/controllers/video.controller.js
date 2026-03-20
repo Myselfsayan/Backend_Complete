76e65faadc83c2c96a6eb85fb16d04849b4eeca2
Myselfsayan <sayanghosh8702@gmail.com>
2026-03-20 20:33:18 +0530

    Implement video filtering and sorting in getAllVideos controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -10,6 +10,29 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+    if (userId) {
+        filter.owner = userId; 
+    }
+
+    if (query) {
+        filter.$or = [
+            { title: { $regex: query, $options: "i" } },//$options: "i" yhis make case insensitive
+            { description: { $regex: query, $options: "i" } }
+        ];
+    }
+
+    let sortOptions = {}
+
+const field = sortBy || "createdAt"
+
+let order = -1
+if (sortType === "asc") {
+    order = 1
+}
+
+sortOptions[field] = order
+
+
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
