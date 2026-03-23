095c9395d0a7498ce429b9f6384d2744fcf8ff1e
Myselfsayan <sayanghosh8702@gmail.com>
2026-03-23 21:05:42 +0530

    Remove duplicate import of asyncHandler in video.controller.js

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,8 +5,6 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
-
 import asyncHandler from "express-async-handler";
 import Video from "../models/video.model.js";
 
