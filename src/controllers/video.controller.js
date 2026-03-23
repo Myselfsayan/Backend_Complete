3b488bab4d686c5d1c1e95cc066ec2c8fefb5ea6
Myselfsayan <sayanghosh8702@gmail.com>
2026-03-23 20:09:13 +0530

    Enhance video upload functionality and add validation for required fields in publishAVideo

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -21,7 +21,8 @@ export const getAllVideos = asyncHandler(async (req, res) => {
     // FILTER
     // ======================
     let filter = {};
-    
+
+
     // Filter by owner (userId)
 
     if (userId) {
@@ -42,7 +43,7 @@ export const getAllVideos = asyncHandler(async (req, res) => {
     let sortOptions = {};
 
     const field = sortBy || "createdAt"; // default sort
-    const order = sortType === "asc" ? 1 : -1;
+    const order = sortType === "asc" ? 1 : -1 ;
 
     sortOptions[field] = order;
 
@@ -79,13 +80,71 @@ export const getAllVideos = asyncHandler(async (req, res) => {
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
+    
     // TODO: get video, upload to cloudinary, create video
+    //Take title, description from req.body and video file from req.files
+    const { title, description} = req.body
+    const { video} = req.files
+    //Check if title + video file exist
+    if(!title || !description){
+        throw new ApiError(400, "Title and description are required")
+    }
+    if(!video ){
+        throw new ApiError(400, "video is required")
+    }
+    //Extract video path from multer (local storage)
+    const videoFilePath = video[0]?.path;
+    // const thumbnailFilePath = req.file?.path;
+    if(!videoFilePath){
+        throw new ApiError(400, "Video file is required")
+    }
+    // Step 2: Send to Cloudinary
+    const videoResult = await cloudinary.uploader.upload(videoFilePath, {
+    resource_type: "video"
+    });
+    // const ThumbnailResult = await cloudinary.uploader.upload(thumbnailFilePath, {
+    // resource_type: "thumbnail"
+    // });
+//Save in DB
+    const newVideo = await Video.create({
+    title: req.body?.title,
+    description: req.body?.description,
+    videoFile : videoResult.secure_url,
+    publicId: videoResult.public_id,
+    owner: req.user._id
+});
+//respond
+res.status(201).json({
+  message: "Video uploaded successfully",
+  video: newVideo
+});
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
+    
     //TODO: get video by id
+
+
+    // Get videoId from params
+    const { videoId } = req.params
+    // Validate it
+    if (!videoId) {
+        throw new ApiError(400, "Video ID is required");
+    }
+    // Find video in DB
+    const video = await Video.findbyId(videoId)
+    // If not found → error
+    if(!video){
+        throw new ApiError(404, "Video not found")
+    }
+    // Return video
+    res
+    .status(200)
+    .json({
+        success : true,
+        video
+    })
+    "Video fetched succesfully"
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
