793fcf23fb02f11177a6ee66d84213ace4fb855d
Myselfsayan <sayanghosh8702@gmail.com>
2026-03-29 10:09:32 +0530

    Implement playlist creation with validation and error handling for name, description, and existence checks

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -7,8 +7,29 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
-
     //TODO: create playlist
+    if(!name || !description || !name.trim() || !description.trim()){
+        throw new ApiError(400, "Name and description are required")
+    }
+    if(!req.user?._id){
+        throw new ApiError(401, "Unauthorized")
+    }
+    const playlistExist = await Playlist.findOne({name : name.trim(), owner : req.user._id})
+    if(playlistExist){
+        throw new ApiError(400, "Playlist with the same name already exists")
+    }
+    // if(!isValidObjectId(req.user._id)){
+    //     throw new ApiError(400, "Invalid user id")
+    // } No need this as we check this in middleware
+    const playlist = await Playlist.create({
+        name : name.trim(),
+        description : description.trim(),
+        owner : req.user._id,
+        videos : []
+    })
+    res
+    .status(201)
+    .json(new ApiResponse(201, playlist, "Playlist created successfully"))
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
